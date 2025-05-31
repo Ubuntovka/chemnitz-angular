7b8ed29c70bdfe61f1ad179c7e43b360b66cee72
Mariia Katsala <ubuntovka@gmail.com>
2025-05-31 22:36:57 +0200

    search bar is working

--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -2,16 +2,21 @@ import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../api.service';
 import * as L from 'leaflet';
 import {MapService} from '../services/map.service';
+import {FormsModule} from '@angular/forms';
 
 
 @Component({
   selector: 'app-searchbar',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './searchbar.component.html',
   styleUrl: './searchbar.component.css'
 })
 export class SearchbarComponent implements OnInit {
   locations: any[] = [];
+  filteredLocations: any[] = [];
+  searchTerm: string = '';
 
   constructor(private apiService: ApiService, private mapService: MapService) {
   }
@@ -27,7 +32,15 @@ export class SearchbarComponent implements OnInit {
   fetchLocations() {
     this.apiService.getLocations().subscribe((data: any[]) => {
       this.locations = data;
+      this.filteredLocations = data;
     });
+  }
+
+  locationsBySearch() {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredLocations = this.locations.filter(loc =>
+      loc.properties?.name?.toLowerCase().includes(term)
+    );
   }
 
 }
