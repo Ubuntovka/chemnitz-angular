1175629770a4320c2e99308a9452cb29dabb818f
Mariia Katsala <ubuntovka@gmail.com>
2025-06-26 15:23:37 +0200

    user ranking component

---
+++ src/app/components/user-ranking/user-ranking.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-user-ranking',
+  imports: [],
+  templateUrl: './user-ranking.component.html',
+  styleUrl: './user-ranking.component.css'
+})
+export class UserRankingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {
+
+  }
+}
