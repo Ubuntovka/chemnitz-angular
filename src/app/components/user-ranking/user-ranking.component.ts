90f8a2fa51cf5253572b5b23062ecd7653c4d9b5
Mariia Katsala <ubuntovka@gmail.com>
2025-06-29 16:51:36 +0200

    ranking design and sorting

--- src/app/components/user-ranking/user-ranking.component.ts
+++ src/app/components/user-ranking/user-ranking.component.ts
@@ -1,14 +1,25 @@
 import {Component, OnInit} from '@angular/core';
 import {ApiService} from '../../services/api.service';
+import {NgClass} from '@angular/common';
+
+
+export interface UserRanking {
+  name: string;
+  ranking: number;
+}
 
 @Component({
   selector: 'app-user-ranking',
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './user-ranking.component.html',
   styleUrl: './user-ranking.component.css'
 })
+
+
 export class UserRankingComponent implements OnInit {
-  usersRankings: any[] = [];
+  usersRankings: UserRanking[] = [];
 
   constructor(
     private apiService: ApiService,
@@ -20,10 +31,20 @@ export class UserRankingComponent implements OnInit {
     this.fetchData();
   }
 
-  fetchData() {
-    this.apiService.getAllUsersAndRankings().subscribe((data: any) => {
-      this.usersRankings = data;
-    })
+  getRankClass(user: any, index: number): string {
+    if (index === 0) return 'first-place';
+    if (index === 1) return 'second-place';
+    if (index === 2) return 'third-place';
+    return '';
   }
+
+  fetchData() {
+    this.apiService.getAllUsersAndRankings().subscribe((data: UserRanking[]) => {
+      this.usersRankings = data.sort((a, b) => b.ranking - a.ranking);
+    });
+  }
+
+
+
 
 }
